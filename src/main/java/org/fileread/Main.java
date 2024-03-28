5a4d51be0edfc8f521dbbc8175d9c3e3ed3843cb
Akshaya Manha Kunnil(UST <245239@ust.com>
2024-03-28 13:21:47 +0530

    Json File handling

---
+++ src/main/java/org/fileread/Main.java
@@ -0,0 +1,8 @@
+package org.fileread;
+
+public class Main {
+    public static void main(String[] args) {
+        JsonFileRead jsonFileRead = new JsonFileRead();
+        jsonFileRead.readContent();
+    }
+}
